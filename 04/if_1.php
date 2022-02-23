c109cef8f3b329641e053e55e2b49efc1c7bbbdd
CHIBA Yukiya <96916006+yukiya119@users.noreply.github.com>
2022-02-23 17:39:13 +0900

    制御構文1の課題完了

---
+++ 04/if_1.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = 2;
+$b = 2;
+
+if ($a > $b) {
+    echo $a;
+} elseif ($a < $b) {
+    echo $b;
+} else {
+    echo '入力された値は等しいです';
+};
---
+++ 04/if_2.php
@@ -0,0 +1,9 @@
+<?php
+
+$point = 4;
+
+if ($point % 2 == 0) {
+    echo $point . 'は偶数です';
+} else {
+    echo $point . 'は奇数です';
+}
